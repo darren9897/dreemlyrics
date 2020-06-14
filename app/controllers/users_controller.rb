d1a27084262069c1a50ed6c08cea6d032ead99c4
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-06-14 19:16:29 -0400

    added user-mvc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     
       def show
         @user = User.find(params[:id])
+    
       end
     
       def new
@@ -13,19 +14,36 @@ class UsersController < ApplicationController
       end
       
       def create
-        @user = User.new
+        @user = User.create(check_params)
+        if @user.valid?
+            redirect_to users_path
+        else
+            flash[:errors] = @user.error.full_messages
+            redirect_to new_user_path
+        end
      end
     
       def edit
         @user = User.find(params[:id])
+       
       end
     
       def update
         @user = User.find(params[:id])
+        @user.update(check_params)
+        redirect_to users_path
       end
     
       def destroy
         user = User.find(params[:id])
+        user.destroy
+
+        redirect_to users_path
       end
 
+      private
+
+      def check_params
+        params.require(:user).permit(:username, :password)
+      end
 end
