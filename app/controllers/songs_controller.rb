88450d44d82abc6e75daaa715400b45a6f48c5d8
darren9897 <darrenkoomson17@gmail.com>
2020-06-14 19:59:32 -0400

    added more seed data for lyrics, created show erb for lyrics and index erb for songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,17 @@
 class SongsController < ApplicationController
+
+    def index
+        @songs = Song.all
+    end
+
+    def show 
+        @song = Song.find(params[:id])
+    end
+
+    
+
+
+
+
+
 end
