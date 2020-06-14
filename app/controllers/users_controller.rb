e7a64dd9fc23895a8b8cf0fdac054b3e783d9adb
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-06-14 14:59:30 -0400

    added user-mvc-test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+    
+      def index
+        @users = User.all
+      end
+    
+      def show
+        @user = User.find(params[:id])
+      end
+    
+      def new
+          @user = User.new
+      end
+      
+      def create
+        @user = User.new
+     end
+    
+      def edit
+        @user = User.find(params[:id])
+      end
+    
+      def update
+        @user = User.find(params[:id])
+      end
+    
+      def destroy
+        user = User.find(params[:id])
+      end
+
 end
